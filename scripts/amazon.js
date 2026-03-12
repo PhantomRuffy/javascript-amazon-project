4301bac1ee54aed5df4dc7db7ada42e42b42882b
PhantomRuffy <francesco.ruffinengo@gmail.com>
2026-03-12 10:08:04 +0100

    cart quantity updates when items are added to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -67,7 +67,7 @@ document.querySelectorAll('.js-add-to-cart')
           matchingItem = item;
         }
       });
-      
+
       if (matchingItem) {
         matchingItem.quantity += 1;
       } else {
@@ -76,5 +76,11 @@ document.querySelectorAll('.js-add-to-cart')
           quantity: 1
         });
       }
+      let cartQuantity = 0;
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
     });
   });
